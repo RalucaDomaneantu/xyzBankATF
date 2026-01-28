8fa80c771faaefadc130b7ce23b6e18a96318051
RalucaDomaneantu <ralucadomaneantu@gmail.com>
2026-01-28 21:31:27 +0200

    New test added

--- src/test/java/tests/CreateCustomersTest.java
+++ src/test/java/tests/CreateCustomersTest.java
@@ -1,36 +1,36 @@
-//package tests;
-//
-//import models.CustomerModel;
-//import org.openqa.selenium.WebDriver;
-//import org.testng.annotations.Test;
-//import pages.CustomerPage;
-//import pages.LoginPage;
-//import pages.ManagerPage;
-//import sharedData.SharedData;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class CreateCustomersTest extends SharedData {
-//    public WebDriver driver;
-//
-//    @Test
-//
-//    public void automationTest() {
-//       // CustomerModel testData = new CustomerModel("CustomersData.json");
-//
-//        LoginPage loginPage = new LoginPage(driver);
-//        loginPage.loginBankManager();
-//
-//        ManagerPage managerPage = new ManagerPage(driver);
-//        managerPage.createCustomer();
-//
-//       List<String> firstNameValueList = Arrays.asList("Raluca1", "Raluca2", "Raluca3");
-//       List<String> lastNameValueList = Arrays.asList("Domaneantu1", "Domaneantu2", "Domaneantu3");
-//       List<String> postCodeValueList = Arrays.asList("1111", "2222", "3333");
-//
-//        CustomerPage customerPage = new CustomerPage(driver);
-//        customerPage.createCustomersProcess(firstNameValueList, lastNameValueList, postCodeValueList);
-//    }
-//}
-//
+package tests;
+
+import models.CustomerModel;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pages.CustomerPage;
+import pages.LoginPage;
+import pages.ManagerPage;
+import sharedData.SharedData;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateCustomersTest extends SharedData {
+
+
+    @Test
+
+    public void automationTest() {
+       // CustomerModel testData = new CustomerModel("CustomersData.json");
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.loginBankManager();
+
+        ManagerPage managerPage = new ManagerPage(getDriver());
+        managerPage.createCustomer();
+
+       List<String> firstNameValueList = Arrays.asList("Raluca1", "Raluca2", "Raluca3");
+       List<String> lastNameValueList = Arrays.asList("Domaneantu1", "Domaneantu2", "Domaneantu3");
+       List<String> postCodeValueList = Arrays.asList("1111", "2222", "3333");
+
+        CustomerPage customerPage = new CustomerPage(getDriver());
+        customerPage.createCustomersProcess(firstNameValueList, lastNameValueList, postCodeValueList);
+    }
+}
+
